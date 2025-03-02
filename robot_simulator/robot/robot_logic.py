1305cd8cb96c7df049b2e707282c7c96f20ffee2
Behnaz <behnaz.shojaei88@gmail.com>
2025-03-02 23:07:21 +0800

    place method defined

--- robot_simulator/robot/robot_logic.py
+++ robot_simulator/robot/robot_logic.py
@@ -15,10 +15,24 @@ class toyRobot:
         self.facing = None
         self.table_size = 5
     
-    def _is_within_bounds(self,x,y):
-        return 0 <= x < self.table_size and 0 <= y < self.table_size
+    def _is_within_bounds(self, x, y):
+         return 0 <= x < self.table_size and 0 <= y < self.table_size
     
-    def place
+
+    def place(self, x, y, facing):
+        
+        facing = facing.upper()
+         
+        if not self._is_within_bounds(x, y):
+            return {"error": f"Invalid position ({x},{y}). Must be within (0-{self.table_size-1}, 0-{self.table_size-1})."}
+        
+        if facing not in self.DIRECTIONS:
+            return {"error": f"Invalid direction '{facing}'. Must be one of {self.DIRECTIONS}."}
+        
+        self.x, self.y, self.facing = x, y, facing
+
+        return {"message": f"Placed at {x},{y},{facing}"}
+
 
     def move
 
@@ -29,3 +43,5 @@ class toyRobot:
     def report
 
     def check command 
+
+    def check origin point
